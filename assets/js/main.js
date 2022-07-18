61a69311f020551d32c4252f611cd62df67c44b3
Minh Vy Ha <vyha0512@gmail.com>
2022-07-18 20:38:39 +1000

    update sonarqube

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,7 @@
 function scrollHeader(){
     const header = document.getElementById('header')
     // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
-    if(this.scrollY >= 50) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
+    if(this.scrollY >= 40) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
 }
 window.addEventListener('scroll', scrollHeader)
 
@@ -121,13 +121,13 @@ themeButton.addEventListener('click', () => {
 const sr = ScrollReveal({
     origin: 'top',
     distance: '60px',
-    duration:2500,
-    delay: 400
+    duration: 2000,
+    delay: 700
     //reset: True,
 })
 sr.reveal(`.home__data`)
 sr.reveal(`.home__handle` , {delay: 700})
-sr.reveal(`.home__socials, .home__scrolls` , {delay: 900, origin:'bottom'})
+sr.reveal(`.home__socials, .home__scrolls` , {delay: 700, origin:'bottom', duration: 2000})
 
 
 
